ce0184d37248937fd48fe34457ee0f433cca3366
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-08-29 17:36:31 -0300

    microsoft classes

---
+++ Projeto/Program.cs
@@ -0,0 +1,8 @@
+﻿string message = "The quick brown fox jumped over the lazy dog";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
