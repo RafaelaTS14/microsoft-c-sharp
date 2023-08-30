5328ecf8cd35c8b43906dd630bcfa83ccf6b2b5e
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-08-30 09:27:00 -0300

    aula sobre matrizes/arrays

---
+++ Matriz/Program.cs
@@ -0,0 +1,58 @@
+﻿//arrays:
+
+/*string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+*/
+
+//foreach
+
+/*string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+//matriz com int
+
+/*int[] inventory = { 200, 450, 700, 175, 250 };
+
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
+*/
+
+//exercício - verificar quais pedidos começam com a letra B no array e exibir
+
+string[] pedidosFraudulentos = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string pedidos in pedidosFraudulentos)
+{
+    if (pedidos.StartsWith("B"))
+    {
+        Console.WriteLine(pedidos);
+    }
+}
