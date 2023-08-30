8155d8d6a403f2a8d9dd4ea942b460d422dbc3b8
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-08-30 09:05:29 -0300

    aula sobre condicionais else if

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -1,8 +1,33 @@
-﻿string message = "The quick brown fox jumped over the lazy dog";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
+﻿using System.Data;
+using System.Linq.Expressions;
 
-if (message.Contains("fox"))
+Random random = new();
+int daysUntilExpiration = random.Next(12);
+int descountPorcentage = 0;
+
+//daysUntilExpiration = 5;
+
+if (daysUntilExpiration == 0)
 {
-    Console.WriteLine("What does the fox say?");
+    Console.WriteLine($"Your subscription has expired");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires within a day!");
+    descountPorcentage = 20;
+    Console.WriteLine($"Renew now and save {descountPorcentage}%");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    descountPorcentage = 10;
+    Console.WriteLine($"Renew now and save {descountPorcentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine(" ");
 }
