9cce0c94420185381924aab2bb023382a65cb2e2
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-12-13 10:44:27 -0300

    conversao

---
+++ conversao/Program.cs
@@ -0,0 +1,126 @@
+﻿//tipos de dados
+
+/*
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+*/
+
+//alterar tipo de dado
+
+/*
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
+*/
+
+//coerção
+
+/*
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+*/
+
+//conversão de expansão ou restrição
+
+/*
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float: {myFloat}");
+*/
+
+//Conversão de dados
+
+/*
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+*/
+
+//Conversao de string pra int usando parse
+
+/*
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
+*/
+
+//string pra int usando convert
+
+/*
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+*/
+
+//comparar a coerção e a conversão de um dec pra um int
+
+/*
+int value = (int)1.5m;
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m);
+Console.WriteLine(value2);
+*/
+
+//tryparse para analise
+
+/*
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine($"Unable to report the measurement.");
+}
+
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+*/
+
+//
+
+/*
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine($"Unable to report the measurement.");
+}
+
+if (result > 0)
+{
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+}
+*/
+
+//desafio
+
+/*string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number;
+    if (decimal)
+}
+*/
