c12fc23e85fd1808464b18ec451283dd306a7f9c
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-12-19 11:37:25 -0300

    array challenges

--- matrizes/Program.cs
+++ matrizes/Program.cs
@@ -72,6 +72,7 @@ foreach (string item in items)
 
 //desafio
 
+/*
 string pangram = "The quick brown fox jumps over the lazy dog";
 
 // Step 1
@@ -90,4 +91,22 @@ for (int i = 0; i < message.Length; i++)
 
 //Step 4
 string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
+Console.WriteLine(result); */
+
+//desafio 2
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
+
+foreach (var item in items)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
